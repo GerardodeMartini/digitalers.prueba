0f723a8c93a1380d84a8cb8790eb37c138e82a25
Gerardo <gerardodemartini@gmail.com>
2025-08-07 19:58:29 -0300

    agrego link en readme

--- 1-enteros.py
+++ 1-enteros.py
@@ -1,3 +1,13 @@
 entero1=10
 entero2=5
 entero3=-3
+
+suma=entero1+entero2
+resta=entero1-entero2
+multiplicacion=entero1*entero2
+division=entero1/entero2
+
+print("Suma: ",suma)
+print("Resta: ",resta)
+print("Multiplicacion: ",multiplicacion)
+print("Division: ",division)                        
